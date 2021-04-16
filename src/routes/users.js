f772b2c1f2b6f36eb9b089898521165b8cd9af34
Facundo Lavagnino/desktop <facuhlavagninodq@gmail.com>
2021-04-16 03:05:26 -0300

    fix errors

--- src/routes/users.js
+++ src/routes/users.js
@@ -15,10 +15,10 @@ const {
 
 const { updatePlan } = require("../controllers/users/updateplan");
 // const { resetPassword } = require("../controllers/users/resetpassword");
-const {
-  sendConfirmPasswordEmail,
-  confirmAccount,
-} = require("../controllers/users/confirmaccount");
+// const {
+//   sendConfirmPasswordEmail,
+//   confirmAccount,
+// } = require("../controllers/users/confirmaccount");
 
 const {
   checkUserIdentity,
@@ -34,7 +34,7 @@ router.get("/:id", getUser);
 router.put("/:id", checkUserIdentity, updateUser);
 router.delete("/:id", checkUserIdentity, deleteUser);
 router.post("/sendconfirmemail", sendConfirmPasswordEmail);
-router.post("/confirmaccount", confirmAccount);
+// router.post("/confirmaccount", confirmAccount);
 // router.post("/forgotpassword", sendResetPasswordEmail);
 // router.post("/resetpassword", resetPassword);
 router.post("/updateplan", updatePlan);
