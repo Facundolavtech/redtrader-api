a2c9622793b5b91ef570e3741e391d587605175f
Facundo Lavagnino/desktop <facuhlavagninodq@gmail.com>
2021-04-16 01:26:37 -0300

    fix errors

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,38 +1,38 @@
-const { Router } = require("express");
-const {
-  createUser,
-  loginUser,
-  authUser,
-  getUser,
-  updateUser,
-  deleteUser,
-} = require("../controllers/users");
+// const { Router } = require("express");
+// const {
+//   createUser,
+//   loginUser,
+//   authUser,
+//   getUser,
+//   updateUser,
+//   deleteUser,
+// } = require("../controllers/users");
 // const {
 //   sendResetPasswordEmail,
 // } = require("../controllers/users/forgotPassword");
-const { updatePlan } = require("../controllers/users/updatePlan");
-const { resetPassword } = require("../controllers/users/resetPassword");
-const {
-  sendConfirmPasswordEmail,
-  confirmAccount,
-} = require("../controllers/users/confirmAccount");
-const {
-  checkUserIdentity,
-  authMiddleware,
-} = require("../middlewares/user.middleware");
+// const { updatePlan } = require("../controllers/users/updatePlan");
+// const { resetPassword } = require("../controllers/users/resetPassword");
+// const {
+//   sendConfirmPasswordEmail,
+//   confirmAccount,
+// } = require("../controllers/users/confirmAccount");
+// const {
+//   checkUserIdentity,
+//   authMiddleware,
+// } = require("../middlewares/user.middleware");
 
-const router = Router();
+// const router = Router();
 
-router.get("/auth", authMiddleware, authUser);
-router.get("/:id", getUser);
-router.post("/register", createUser);
-router.post("/login", loginUser);
-router.post("/sendconfirmemail", sendConfirmPasswordEmail);
-router.post("/confirmaccount", confirmAccount);
+// router.get("/auth", authMiddleware, authUser);
+// router.get("/:id", getUser);
+// router.post("/register", createUser);
+// router.post("/login", loginUser);
+// router.post("/sendconfirmemail", sendConfirmPasswordEmail);
+// router.post("/confirmaccount", confirmAccount);
 // router.post("/forgotpassword", sendResetPasswordEmail);
-router.post("/resetpassword", resetPassword);
-router.post("/updateplan", updatePlan);
-router.put("/:id", checkUserIdentity, updateUser);
-router.delete("/:id", checkUserIdentity, deleteUser);
+// router.post("/resetpassword", resetPassword);
+// router.post("/updateplan", updatePlan);
+// router.put("/:id", checkUserIdentity, updateUser);
+// router.delete("/:id", checkUserIdentity, deleteUser);
 
-module.exports = router;
+// module.exports = router;
