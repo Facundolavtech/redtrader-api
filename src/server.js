94c7185cfdb07566bd47948406d00062e713dfb2
root <root@redtrader-api.com>
2021-05-17 05:09:57 +0000

    production

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const cron = require("node-cron");
 const deletePlan = require("../src/tasks/deletePlan");
@@ -8,14 +7,28 @@ const node_media_server = require("./nms/media_server");
 require("./config/database");
 require("dotenv").config();
 
+const https = require('https');
+const fs = require('fs')
+const key = fs.readFileSync(__dirname + '/../private.key');
+const cert = fs.readFileSync(__dirname + '/../certificate.crt');
+const options = {
+  key: key,
+  cert: cert
+};
+
+const app = express();
+
+const server = https.createServer(options, app);
+
 module.exports = function () {
+
   node_media_server.run();
 
   //Middlewares
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json({ extended: true }));
   app.use(cors());
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 9443;
 
   cron.schedule("0 * * * *", function () {
     console.log("Deleting expire plans");
@@ -33,7 +46,7 @@ module.exports = function () {
   app.use("/api/lives/educator", require("./routes/lives/educator"));
 
   //Run server
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server on port ${port}`);
   });
 };
